fc36d290684a25fcc8a52180267780a796d5b259
unknown <benorule@outlook.com>
2020-09-21 20:04:48 +0930

    add mongodb cloud driver code to server.js

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = ("mongodb+srv://benorule:password01@cluster0.iff4h.mongodb.net/budget?retryWrites=true&w=majority");
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 const PORT = 3000;
 
 const app = express();
